5ab34e0fa8d60e2008d9232d04cb2ae6a1a625fc
Szobonya Dávid <szobonya.david@gmail.com>
2024-08-09 20:57:30 +0200

    login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  loading = false;
 
+  constructor(private _router: Router, private _fb: FormBuilder) {
+    this.loginForm = this._fb.group(
+      {
+        email: ['', [Validators.required]],
+        password: ['', [Validators.required]]
+      });
+  }
+
+  getFieldValue(field: string): any {
+    return this.loginForm.get(field)?.value;
+  }
+
+  login() {
+    if (this.loginForm.invalid) return;
+
+    this.loading = true;
+
+    // this._authService.login(this.getFieldValue('email'), this.getFieldValue('password')).then(_ => {
+    //   this.loading = false;
+    //   this._router.navigateByUrl('/');
+    // }).catch(error => {
+    //   console.error(error);
+    // });
+  }
 }
